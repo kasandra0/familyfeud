f619e0a61ce10a8756af5fe6c4eb16eb0b2c07a3
Marcus Beall <marcbeall@gmail.com>
2018-11-30 11:39:14 -0700

    :frame_photo:

--- server/models/survey.js
+++ server/models/survey.js
@@ -6,7 +6,8 @@ let name = 'Survey'
 let schema = new Schema({
   question: { type: String, required: true },
   answers: { type: [String, Number], required: true },
-  userId: { type: ObjectId, ref: 'uid', required: true }
+  userId: { type: ObjectId, ref: 'uid', required: true },
+  img: { type: String, required: true }
 })
 
 let model = mongoose.model(name, schema)
