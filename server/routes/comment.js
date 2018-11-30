362d19618d740a4db2449e2c2c173fd3b58b671e
Marcus Beall <marcbeall@gmail.com>
2018-11-30 12:19:39 -0700

    edited routes/added get subcomments

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -1,16 +1,10 @@
 let router = require('express').Router()
 let Comments = require('../models/comment')
+let Subcomment = require('../models/subcom')
 
-//delete a comment, uses the comment id as the url id parameter (different from above)
+//delete a comment, uses the comment id as the url id parameter
 router.delete('/:id', (req, res, next) => {
   Comments.findByIdAndDelete(req.params.id)
-    .then(Comments => res.send(Comments))
-    .catch(next)
-})
-
-//create a comment, uses the survey id as the url id parameter (different from above)
-router.post('/:id/comment', (req, res, next) => {
-  Comments.create(req.params.id, req.body)
     .then(Comments => res.send(Comments))
     .catch(next)
 })
@@ -26,6 +20,13 @@ router.put('/:id/up', (req, res, next) => {
 router.put('/:id/down', (req, res, next) => {
   Comments.findByIdAndUpdate(req.params.id, { downvotes: req.body.downvotes })
     .then(Comments => res.send(Comments))
+    .catch(next)
+})
+
+//create a subcomment, uses the comment id as the url id parameter
+router.post('/:id/subcomment', (req, res, next) => {
+  Subcomment.create({ comId: req.params.id }, req.body)
+    .then(Subcomment => res.send(Subcomment))
     .catch(next)
 })
 
