9b12b98d6912ad7e36b5fe8372bbc7a977dac80c
Marcus Beall <marcbeall@gmail.com>
2018-11-30 17:15:10 -0700

    moved stuff

--- public/app/components/survey/survey-service.js
+++ public/app/components/survey/survey-service.js
@@ -3,6 +3,13 @@ let _surveyApi = axios.create({
   baseURL: '/api/surveys',
   withCredentials: true
 })
+
+let _wordApi = axios.create({
+  baseURL: 'http://words.bighugelabs.com/api/2/b6c49817eae10a33a2d79f4760dba24f/word/json',
+})
+let words = []
+
+
 function handleError(error) {
   throw new Error(error)
 }
@@ -22,5 +29,19 @@ export default class SurveyService {
     _surveyApi.post('', formData)
       .then(callback)
       .catch(handleError)
+  }
+  checkAnswer(answer, answers) {
+    function (answer) {
+      _wordApi.get(answer)
+        .then(res => { words = res.data })
+        .catch(console.error(err)
+        )
+    }
+    words.forEach(word => {
+      if (word == answers) {
+        return true
+      }
+    })
+    return false
   }
 }
